25efa4d92747e188af0970033a609429963b9f9c
dmitrijtkacenko <tkachenkofindir@gmail.com>
2025-09-30 11:20:10 +0300

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,12 @@
+import React from 'react';
+import './Header.css'; // если есть стили
+
 function Header() {
-    return <div></div>; // ✅ безопасно
-  }
-  
+  return (
+    <header className="header">
+      <h1>Добро пожаловать!</h1>
+    </header>
+  );
+}
+
+export default Header;
