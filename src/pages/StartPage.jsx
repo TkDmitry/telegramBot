bb4e3bbea3f208144aae961e82ec8026243c5534
dmitrijtkacenko <tkachenkofindir@gmail.com>
2025-10-01 22:58:18 +0300

    add

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from '../components/NavBar/NavBar';
 import './StartPage.css';
 
 function StartPage() {
